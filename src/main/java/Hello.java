a971332fdb0e87e526e959e288cb3acb3d0a57ca
Fahrettin Gedik <fahged41@gmail.com>
2024-12-29 23:41:09 +0100

    I put the curly brackets.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,12 @@
 public class Hello {
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
         boolean isAlien = false;
-        if (!isAlien)
+        if (!isAlien) {
             System.out.println("You are not an alien!");
+            System.out.println("And I am scared of aliens too");
+        }
     }
 }

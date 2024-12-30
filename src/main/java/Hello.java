132c96d3632f20f8c999b082d6b9a82a6c31eb77
Fahrettin Gedik <fahged41@gmail.com>
2024-12-30 23:50:24 +0100

    Assignment operator and equal operator, boolean

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -15,9 +15,29 @@ public class Hello {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100) {
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than secondTopScore and less than 100!");
         }
+
+        if((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if(newValue == 50) {
+            System.out.println("The value is 50");
+        } else {
+            System.out.println("The value is not 50");
+        }
+
+        boolean isCar = false;
+        if (isCar == true) {
+            System.out.println("You are driving a car");
+        } else {
+            System.out.println("You are not driving a car");
+        }
+
+
     }
 }
